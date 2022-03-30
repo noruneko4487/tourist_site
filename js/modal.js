2cf0c273dbfdc2df677900df34dd30e14e1c7f25
kyu6479 <kyu6479@gmail.com>
2022-03-30 15:15:25 +0900

    add modal js

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const modal = document.querySelector('.campain-modal');
 const closeMark = document.querySelector('.fa-xmark');
 
@@ -17,23 +16,3 @@ closeMark.onclick = modalHide;
 
 
 
-=======
-const modal = document.querySelector('.campain-modal');
-const closeMark = document.querySelector('.fa-xmark');
-
-setTimeout(() => {
-    modal.style.display = 'block';
-}, 1000);
-
-function modalHide(){
-    modal.style.display = 'none';
-}
-
-closeMark.onclick = modalHide;
-
-
-
-
-
-
->>>>>>> 04e1d4c54c0694f7dfb393b1eb28ea3353bf7fd8
