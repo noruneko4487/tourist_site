b7ee2b674b19e32522935b915a77db973865ae50
kyu6479 <kyu6479@gmail.com>
2022-03-31 16:23:04 +0900

    navmenu control

--- js/navmenu.js
+++ js/navmenu.js
@@ -1,16 +1,56 @@
 const navBtn = document.querySelector('.header-wrapper__menu-btn');
 const navOpen = document.querySelector('.nav');
+const targetLink = document.querySelectorAll('.nav__list__anchor');
 const closeMask = document.querySelector('.mask');
 
 function menuDisp(){
     navOpen.classList.toggle('menu-open');
+    const links = [
+        '#',
+        '#',
+        '#',
+        '#',
+        '#',
+    ];
+
+    
+
+    links.forEach(function (pageLink){
+        targetLink.forEach(function(targetAdd){
+            targetAdd.setAttribute('href', pageLink);
+        });
+    });
+
+    
+
+    
 }
 
 function close(){
-    navOpen.classList.remove('menu-open')
+    navOpen.classList.remove('menu-open');
+    targetLink.forEach(function(targetRm){
+        targetRm.removeAttribute('href');
+    });
 }
 
-navBtn.addEventListener('click', menuDisp);
+
+
+let counter = 0;
+navBtn.addEventListener('click', () => {
+
+
+    
+    counter++;
+    if(counter % 2 !== 0){
+        menuDisp();
+
+    }else{
+        close();
+    }
+
+});
+
+
 closeMask.addEventListener('click', close);
 
 
